da593321d673c8d0a3ab7989ba85777b8b7a0f8d
Tushar Kanti Sarkar <tksarkar@isystango.com>
2018-03-27 16:35:36 +0530

    Method name updated.

--- ios/RNAuthorizeNet.m
+++ ios/RNAuthorizeNet.m
@@ -24,12 +24,16 @@ static NSString *const DATA_VALUE = @"DATA_VALUE";
 
 
 
-RCT_EXPORT_METHOD(payWithAuthorizeNet:(NSDictionary *)paymentValues isProduction:(BOOL)isProduction  callBack:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(getTokenWithRequestForCard:(NSDictionary *)cardValues isProduction:(BOOL)isProduction  callBack:(RCTResponseSenderBlock)callback)
 {
     AcceptSDKHandler *handler = [[AcceptSDKHandler alloc] initWithEnvironment:isProduction?AcceptSDKEnvironmentENV_LIVE:AcceptSDKEnvironmentENV_TEST];
     AcceptSDKRequest *request = [[AcceptSDKRequest alloc] init];
-    request.merchantAuthentication.name =[paymentValues valueForKey:LOGIN_ID];
-    request.merchantAuthentication.clientKey =[paymentValues valueForKey:CLIENT_KEY];    request.securePaymentContainerRequest.webCheckOutDataType.token.cardNumber =[paymentValues valueForKey:CARD_NO];    request.securePaymentContainerRequest.webCheckOutDataType.token.expirationMonth = [paymentValues valueForKey:EXPIRATION_MONTH];    request.securePaymentContainerRequest.webCheckOutDataType.token.expirationYear = [paymentValues valueForKey:EXPIRATION_YEAR];    request.securePaymentContainerRequest.webCheckOutDataType.token.cardCode = [paymentValues valueForKey:CVV_NO];
+    request.merchantAuthentication.name =[cardValues valueForKey:LOGIN_ID];
+    request.merchantAuthentication.clientKey =[cardValues valueForKey:CLIENT_KEY];
+    request.securePaymentContainerRequest.webCheckOutDataType.token.cardNumber =[cardValues valueForKey:CARD_NO];
+    request.securePaymentContainerRequest.webCheckOutDataType.token.expirationMonth = [cardValues valueForKey:EXPIRATION_MONTH];
+    request.securePaymentContainerRequest.webCheckOutDataType.token.expirationYear = [cardValues valueForKey:EXPIRATION_YEAR];
+    request.securePaymentContainerRequest.webCheckOutDataType.token.cardCode = [cardValues valueForKey:CVV_NO];
     [handler getTokenWithRequest:request successHandler:^(AcceptSDKTokenResponse * _Nonnull token) {
         //NSLog(@"success %@", token.getOpaqueData.getDataValue);
         NSDictionary *responsDict = @{DATA_DESCRIPTOR:token.getOpaqueData.getDataDescriptor,DATA_VALUE:token.getOpaqueData.getDataValue};
